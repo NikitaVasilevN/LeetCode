975783609bfd29bc751aa39855ae1bda16b3c544
Nikita <nikita31415@gmail.com>
2024-07-11 18:52:49 +0300

    task 69 v2

--- 69_Sqrt(x).py
+++ 69_Sqrt(x).py
@@ -23,9 +23,15 @@
 
 class Solution:
     def mySqrt(self, x: int) -> int:
-        i = 0
-        while True:
-            if i*i <= x and (i+1)*(i+1) > x:
-                return i
+        left = 0
+        right = x
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid < x:
+                left = mid + 1
+            elif mid * mid > x:
+                right = mid -1
             else:
-                i+=1
+                return mid
+            
+        return right
