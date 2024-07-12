f40dd7b639b05d3a3c30b5349b11682685a534b8
Nikita <nikita31415@gmail.com>
2024-07-12 13:20:22 +0300

    task 171

---
+++ 171_Excel_Sheet_Column_Number.py
@@ -0,0 +1,42 @@
+# Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
+
+# For example:
+
+# A -> 1
+# B -> 2
+# C -> 3
+# ...
+# Z -> 26
+# AA -> 27
+# AB -> 28 
+# ...
+ 
+
+# Example 1:
+
+# Input: columnTitle = "A"
+# Output: 1
+# Example 2:
+
+# Input: columnTitle = "AB"
+# Output: 28
+# Example 3:
+
+# Input: columnTitle = "ZY"
+# Output: 701
+ 
+
+# Constraints:
+
+# 1 <= columnTitle.length <= 7
+# columnTitle consists only of uppercase English letters.
+# columnTitle is in the range ["A", "FXSHRXW"].
+
+
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        num = 0
+        for i in range(1, len(columnTitle) + 1):
+            num += 26 ** (i-1) * (ord(columnTitle[-i].lower()) - 96)
+        return num
+
